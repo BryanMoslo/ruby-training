cfc9e8fb8a17cc92ade336f8da486dd669bbad5a
Antonio Pagano <apagano@wawand.co>
2015-08-08 16:21:43 -0500

    [cleaning] moving constants to the Plate:: scope

--- lib/plates_comparator.rb
+++ lib/plates_comparator.rb
@@ -1,9 +1,7 @@
 module Plates
+  ALPHABET      = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  INDEX_VALUES  = [ 676000, 26000, 1000, 100, 10, 1 ]
   class Comparator
-
-    ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    VALUE_PER_POSITION = [ 676000, 26000, 1000, 100, 10, 1 ]
-
     def self.plate_to_decimal(plate)
       throw ArgumentError unless Plates::Validator.valid? plate
 
@@ -14,12 +12,12 @@ module Plates
 
     def self.char_value(index, char)
       char_index =  if index < 3
-                      ALPHABET.index(char)
+                      Plates::ALPHABET.index(char)
                     else
                       char.to_i
                     end
-      index_unit_value = VALUE_PER_POSITION[index]
-      char_index * index_unit_value
+
+      char_index * Plates::INDEX_VALUES[index]
     end
 
     def self.distance_between(plate_a, plate_b)
